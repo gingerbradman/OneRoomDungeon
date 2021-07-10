b5a9d7f9e36959f8940c39d6ac9a6e23d880940a
gingerbradman <lordbreadly8@gmail.com>
2021-07-10 00:39:17 -0500

    Basic player movement and character sprite I made watching black widow

---
+++ Assets/Scripts/PlayerMovement_script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement_script : MonoBehaviour
+{
+
+    public float speed = 100;
+    public Transform obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frameaw
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector3(h, v, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+
+        obj.transform.position += tempVect;
+    }
+}
